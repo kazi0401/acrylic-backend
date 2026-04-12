713ad493e66aa02f6426dbc3bb3626a8b714773d
Allison-Yeh <allison.yeh@gmail.com>
2026-04-12 15:22:47 -0700

    feat: Add user auth and songs API with DRF
    - JWT-based registration/login
    - Song CRUD endpoints
    - Role-based user model

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Acrylic backend is running.')
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('api/', include('songs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
