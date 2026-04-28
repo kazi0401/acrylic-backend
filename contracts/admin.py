000ef178519e01c93abe845c9f0c2d262c62389f
Tanzir Kazi <kazit@uci.edu>
2026-04-27 21:18:12 -0700

    Added panel for editing contracts in the django admin panel

--- contracts/admin.py
+++ contracts/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Contract
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ['user', 'contract_type', 'status', 'version', 'signed_at', 'expires_at']
+    list_filter = ['status', 'contract_type']
